544ed9004125cb56d6bd0e45ca9e8dd5b830f418
tglspfdsfgs <tglspfdsfgs@gmail.com>
2022-02-22 18:15:04 +0200

    Finished shop.html

--- scripts/footermapscall.js
+++ scripts/footermapscall.js
@@ -1,11 +1,4 @@
-var button = document.querySelector(".call-map-btn");
 var popup_map = document.querySelector(".modal-map");
-
-button.addEventListener("click", function( evt ) {
-	evt.preventDefault();
-	popup_map.classList.add("modal-show");
-});
-
 var close = popup_map.querySelector(".modal-close");
 
 close.addEventListener("click", function ( evt) {
